baba3646893d86cc710c9304ebd65748a6b84173
Nikita Sherstnev <nsherstnev@enfint.tech>
2023-10-14 13:52:36 +0200

    Add gfpgan enhancer test

--- tests/test_swap.py
+++ tests/test_swap.py
@@ -4,16 +4,30 @@ import pathlib
 import pytest
 import numpy as np
 
+import cv2
 from settings import Settings
 from roop.face_util import extract_face_images
 from roop.core import batch_process
 import roop.globals
 from roop.ProcessEntry import ProcessEntry
 from roop.FaceSet import FaceSet
+from roop.ProcessMgr import ProcessMgr
+from roop.ProcessOptions import ProcessOptions
 
 
 def mock_progress_gradio(*args, **kwargs):
     pass
+
+
+class MockInsightFace:
+    processorname = 'faceswap'
+    type = 'swap'
+
+    def __init__(self, fake_frame):
+        self.fake_frame = fake_frame
+
+    def Run(self, *args):
+        return self.fake_frame
 
 
 class TestSwapFace:
@@ -28,7 +42,7 @@ class TestSwapFace:
         roop.globals.output_path = 'output'
         roop.globals.face_swap_mode = 'first'
 
-    def test_swap(self):
+    def test_swap_one_face(self):
         source = 'tests/assets/ana-de-armas.jpg'
         target = 'tests/assets/wonder-woman.jpg'
 
@@ -52,9 +66,10 @@ class TestSwapFace:
 
         batch_process(list_files_process, use_clip, clip_text,
                       processing_method == "In-Memory processing", mock_progress_gradio)
-        is_processing = False
+
         outdir = pathlib.Path(roop.globals.output_path)
-        result = [item for item in outdir.rglob(f"*{target.split('/')[-1].split('.')[0]}*") if item.is_file()][0]
+        result = [item for item in outdir.rglob(f"*{target.rsplit('/', maxsplit=1)[-1].split('.')[0]}*") \
+                  if item.is_file()][0]
 
         swapped_face = extract_face_images(str(result), (False, 0))
 
@@ -63,3 +78,40 @@ class TestSwapFace:
 
         assert source_similarity > target_similarity * 10
         os.remove(result)
+
+    def test_GFPGAN_enhancement(self):
+        source = 'tests/assets/ana-de-armas.jpg'
+        target = 'tests/assets/wonder-woman.jpg'
+        fake_frame = 'tests/assets/fake-frame.jpg'
+
+        enhanced_path = 'tests/enhanced.jpg'
+
+        source_face = extract_face_images(source, (False, 0))[0][0]
+        source_face.mask_offsets = (0,0)
+
+        face_set = FaceSet()
+        face_set.faces.append(source_face)
+
+        target_face = extract_face_images(target, (False, 0))[0][0]
+        target_face.matrix = np.array([[ 4.28439966e-01, -2.93581036e-02, -2.89326694e+01],
+                                      [ 2.93581036e-02,  4.28439966e-01, -3.45334194e+01]])
+
+        fake_frame = cv2.imread(fake_frame)
+        target_img = cv2.imread(target)
+
+        process_mgr = ProcessMgr(None)
+        options = ProcessOptions('gfpgan', roop.globals.distance_threshold,
+                                 roop.globals.blend_ratio, 'first', 0, None)
+        process_mgr.initialize([face_set], [target_face], options)
+
+        process_mgr.processors.insert(0, MockInsightFace(fake_frame))
+
+        newframe = process_mgr.process_face(0, target_face, target_img)
+
+        cv2.imwrite(enhanced_path, newframe)
+
+        file_size1 = os.path.getsize('tests/assets/ww-swap.jpg')
+        file_size2 = os.path.getsize(enhanced_path)
+
+        assert file_size1 < file_size2
+        os.remove(enhanced_path)
