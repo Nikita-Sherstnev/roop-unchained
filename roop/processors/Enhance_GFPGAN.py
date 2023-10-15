a4741cb6f60a87d1d3d283c4365875dfdee3869b
Nikita Sherstnev <nik124854@gmail.com>
2023-10-15 18:27:08 +0200

    Remove gfpgan dependency

--- roop/processors/Enhance_GFPGAN.py
+++ roop/processors/Enhance_GFPGAN.py
@@ -2,11 +2,11 @@ from typing import Any, List, Callable
 import cv2
 import numpy as np
 import roop.globals
-import gfpgan
+import torch
 
 from roop.typing import Face, Frame, FaceSet
 from roop.utilities import resolve_relative_path
-
+from roop.models.gfpgan import GFPGANer
 
 # THREAD_LOCK = threading.Lock()
 
@@ -24,7 +24,8 @@ class Enhance_GFPGAN():
     def Initialize(self, devicename):
         if self.model_gfpgan is None:
             model_path = resolve_relative_path('../models/GFPGANv1.4.pth')
-            self.model_gfpgan = gfpgan.GFPGANer(model_path=model_path, upscale=4)
+            self.device = self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            self.model_gfpgan = GFPGANer(model_path=model_path, upscale=4, device=self.device)
             # replace Mac mps with cpu for the moment
             devicename = devicename.replace('mps', 'cpu')
             self.devicename = devicename
@@ -34,10 +35,8 @@ class Enhance_GFPGAN():
     def Run(self, source_faceset: FaceSet, target_face: Face, temp_frame: Frame) -> Frame:
         input_size = temp_frame.shape[1]
 
-        _, _, result = self.model_gfpgan.enhance(
-            temp_frame,
-            paste_back=True
-        )
+        cropped_face = cv2.resize(temp_frame, (512, 512), interpolation=cv2.INTER_LINEAR)
+        result = self.model_gfpgan.enhance(cropped_face)
 
         scale_factor = int(result.shape[1] / input_size)
         return result.astype(np.uint8), scale_factor
